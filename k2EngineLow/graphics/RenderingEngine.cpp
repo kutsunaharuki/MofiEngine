0b0494d69d5b8c1a23908c0d393e90d16bab6466
kutsunaharuki <ca01254@st.kawahara.ac.jp>
2026-07-22 18:36:28 +0900

    Step2-1　RenderingEngineの作成

--- k2EngineLow/graphics/RenderingEngine.cpp
+++ k2EngineLow/graphics/RenderingEngine.cpp
@@ -10,12 +10,72 @@ namespace nsK2EngineLow
 {
 	void RenderingEngine::Execute(RenderContext& rc)
 	{
+		//=====================================================
+		// パス1: シャドウマップパス
+		//=====================================================
+		// 書き込める状態になるまで待つ
+		rc.WaitUntilToPossibleSetRenderTarget(m_shadowMap);
+		// 描き先をシャドウマップに切り替え
+		rc.SetRenderTargetAndViewport(m_shadowMap);
+		// 白でクリア(Create で渡したクリア色)
+		rc.ClearRenderTargetView(m_shadowMap);		
+
+		for (auto* model : m_shadowCasters)
+		{
+			// ライト視点から描画する
+			model->Draw(rc, m_lightCamera);
+		}
+		// シャドウキャスターリストをマイフレーム空にする
+		m_shadowCasters.clear();
+
+		// 描き終わるまで待つ → 以後テクスチャとして読める
+		rc.WaitUntilFinishDrawingToRenderTarget(m_shadowMap);
+
+		// 描き先を画面(フレームバッファ)に戻して、パス2のフォワードレンダリングパスに移行する
+		g_graphicsEngine->ChangeRenderTargetToFrameBuffer(rc);
+		
+
+
+		rc.SetViewportAndScissor(g_graphicsEngine->GetFrameBufferViewport());
+
 		// 登録されたモデルを全部描画する
 		for (auto* model : m_models)
 		{
+			// カメラ視点で描画する
 			model->Draw(rc);
 		}
-		// マイフレームリストを空にする
+		// 毎フレームリストを空にする
 		m_models.clear();
+	}
+
+
+	void RenderingEngine::InitializeLight()
+	{
+		// 長いから省略
+		auto& scLight = SceneLight::GetInstance();
+
+		// シーンライトからディレクションライトの向きを取得してくる
+		const Vector3 lightPos = Vector3(500.0f, 500.0f, 500.0f);
+		const Vector3 targetPos = Vector3::Zero;
+		Vector3 lightDir = targetPos - lightPos;
+		lightDir.Normalize();
+		scLight.SetDirection(lightDir);
+
+		// ライトの逆側・上空に置く
+		m_lightCamera.SetPosition(lightDir * -1000.0f);
+		// キャラのいるあたりを見る
+		m_lightCamera.SetTarget(Vector3::Zero);
+		// 真下を向くときはY以外の値を上げる(Yだと視線と平行になり行列が壊れるから)
+		m_lightCamera.SetUp({ 1.0f,0.0f,0.0f });
+		// 平行投影
+		m_lightCamera.SetUpdateProjMatrixFunc(Camera::enUpdateProjMatrixFunc_Ortho);
+		// 影を写す範囲の幅
+		m_lightCamera.SetWidth(SHADOW_WIDTH);
+		// 影を写す範囲の高さ(この範囲外には影はできない)
+		m_lightCamera.SetHeight(SHADOW_HEIGHT);
+		// ライトカメラの更新
+		m_lightCamera.Update();
+
+		scLight.SetLightProjMatrix(m_lightCamera.GetViewProjectionMatrix());
 	}
 }
--- k2EngineLow/graphics/RenderingEngine.h
+++ k2EngineLow/graphics/RenderingEngine.h
@@ -1,12 +1,28 @@
 /**
  * @file RenderingEngine.h
  * @brief レンダリングエンジン
+ * @details: Step2
  */
 #pragma once
 
 
 namespace nsK2EngineLow
 {
+	namespace
+	{
+		/** ライトの向きに乗算させる数 */
+		constexpr float LIGHT_HEIGHT = -1000.0f;
+		/** 影の幅 */
+		constexpr float SHADOW_WIDTH = 2000.0f;
+		/** 影の高さ */
+		constexpr float SHADOW_HEIGHT = 2000.0f;
+		/** カメラの向き */
+		const Vector3 CAMERA_DIR = { 1.0f,0.0f,0.0f };
+		/** ライトの位置 */
+		const Vector3 LIGHT_POSITION{ 0.0f,600.0f,0.0f };
+	}
+
+
 	class RenderingEngine
 	{
 	public:
@@ -42,7 +58,7 @@ namespace nsK2EngineLow
 		 * @brief シャドウキャスターをリストに追加
 		 * @param model シャドウモデル
 		 */
-		void AddShadowCaster(Model& model) { m_shadowMapModels.push_back(&model); }
+		void AddShadowCaster(Model& model) { m_shadowCasters.push_back(&model); }
 		/**
 		 * @brief ライトカメラを取得
 		 * @return ライトカメラ
@@ -55,20 +71,37 @@ namespace nsK2EngineLow
 		 */
 		Texture& GetShadowMapTexture() { return m_shadowMap.GetRenderTargetTexture(); }
 
+		/**
+		 * @brief ライトカメラの初期化
+		 */
+		void InitializeLight();
+
 
 	private:
 		/**
 		 * @brief コンストラクタ
 		 * @details: シングルトンパターンを採用しているため、外部からのインスタンス化を禁止するためにprivateにしている
 		 */
-		RenderingEngine() {}
+		RenderingEngine() 
+		{
+			// シャドウマップのクリアカラーを白に設定
+			float clearColor[4] = { 1.0f,1.0f,1.0f,1.0f };
+			// シャドウマップのレンダリングターゲットを作成
+			m_shadowMap.Create(
+				1024,1024,                        // 解像度
+				1,1,							  // ミップマップ数、配列数(1でよい)
+				DXGI_FORMAT_R8G8B8A8_UNORM,		  // カラーのフォーマット
+				DXGI_FORMAT_D32_FLOAT,			  // 深度バッファのフォーマット
+				clearColor						  // シャドウマップのクリアカラー
+			);
+		}
 
 		/** ライトカメラ */
 		Camera m_lightCamera;
 		/** シャドウマップ */
 		RenderTarget m_shadowMap;
 		/** 影を落とすモデル(毎フレーム登録) */
-		std::vector<Model*> m_shadowMapModels;
+		std::vector<Model*> m_shadowCasters;
 		/** モデルのリスト */
 		std::vector<Model*> m_models;
 	};
